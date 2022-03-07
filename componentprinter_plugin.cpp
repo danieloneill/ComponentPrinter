8538b9bc164b3ba49e996d5eb2d07e1d601a5096
Daniel O'Neill <daniel@oneill.app>
2022-03-07 02:36:26 -0700

    Incremented component version in the off-chance somebody will be running both versions in a Qt5.x installation.

--- componentprinter_plugin.cpp
+++ componentprinter_plugin.cpp
@@ -6,7 +6,7 @@
 void ComponentPrinterPlugin::registerTypes(const char *uri)
 {
     // @uri com.foxmoxie.Printer
-    qmlRegisterType<Printer>(uri, 1, 1, "Printer");
+    qmlRegisterType<Printer>(uri, 1, 2, "Printer");
 }
 
 
